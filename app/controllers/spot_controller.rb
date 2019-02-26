e6dfc376339eb69f059982c0ae19e7253460588b
Alice Brewer <alicebrewer09@gmail.com>
2019-02-26 18:05:27 +0100

    added spot controller

---
+++ app/controllers/spot_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # before_action :set_painting, only: [:show, :edit, :update, :destroy]
+  # skip_before_action :authenticate_user!, only: [:edit, :index, :show]
+  # skip_after_action :verify_authorized, only:[:update, :search, :index]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+end
