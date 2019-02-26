611e2d08e801acccb76aa18f4c5e3120c331563f
Antoine Patouillaud <antoine.patouillaud@gmail.com>
2019-02-26 18:39:29 +0100

    update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,13 @@ class Profile < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  LEVELS = ["1-Beginner", "2-Intermediate", "3-Advanced", "Expert")
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :author, presence: true
+  validates :level,
+            presence: true,
+            inclusion: { in: LEVELS, message: "Part of the list" }
 end
