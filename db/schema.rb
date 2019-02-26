dea9b36473b250046d0cdf164e378643175e7463
Antoine Patouillaud <antoine.patouillaud@gmail.com>
2019-02-26 18:27:16 +0100

    update de tous les models avec les bonnes colonnnes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_170356) do
+ActiveRecord::Schema.define(version: 2019_02_26_172213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,9 @@ ActiveRecord::Schema.define(version: 2019_02_26_170356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "seabed"
+    t.string "best_tide"
+    t.integer "difficulty_level"
   end
 
   create_table "users", force: :cascade do |t|
