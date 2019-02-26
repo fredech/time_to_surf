daf9b758c173ef64f4643310c0e6ea518e29d4b0
Frederic Chochon <45423324+fredech@users.noreply.github.com>
2019-02-26 11:35:37 +0100

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :session
 
-  RATES = %w(0 1 2 3 4 5)
+  #RATES = %w(0 1 2 3 4 5)
   validates :content, presence: true
-  validates :rating,
-            presence: true,
-            inclusion: { in: RATES, message: "Please choose a rating between 0 to 5" }
+  #validates :rating,
+  #         presence: true,
+  #         inclusion: { in: RATES, message: "Please choose a rating between 0 to 5" }
 end
