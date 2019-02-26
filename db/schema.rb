572356f781aa561a70c786a7d3ab9d6f5c109e91
Alice Brewer <alicebrewer09@gmail.com>
2019-02-26 13:04:49 +0100

    added photo col to review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_173443) do
+ActiveRecord::Schema.define(version: 2019_02_26_120349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_173443) do
     t.bigint "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["session_id"], name: "index_reviews_on_session_id"
   end
 
