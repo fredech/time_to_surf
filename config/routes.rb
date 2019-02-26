1d4a74ad17cf2f784a2051f4fab6bda1d1c469bf
Alice Brewer <alicebrewer09@gmail.com>
2019-02-26 11:56:46 +0100

    initial routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :spots, only: [ :index, :show] do
+    resources :preferred_spots, only: [ :create, :destroy ]
+  end
+  resources :sessions, only: [ :index, :create, :edit, :update, :destroy ] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  get "/dashboard", to: "pages#dashboard", as: :dashboard
 end
