9e4827bae854c7e2fd5d4e66b293810b6f8eb2be
damien arnaud <darnaud@mail.com>
2019-03-02 02:20:39 +0100

    searchbar styled

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
+import "flatpickr/dist/themes/airbnb.css"
 
 flatpickr(".datepicker", {
   enableTime: true,
+  time_24hr: true,
   minDate: "today",
+  maxDate: new Date().fp_incr(3),
+  defaultHour: 12,
+  defaultMinute: 0,
 })
