3ad8ba3708690b688a11f8b34976428497601d82
damien arnaud <darnaud@mail.com>
2019-03-04 17:22:16 +0100

    home v2 done

--- app/services/search.rb
+++ app/services/search.rb
@@ -11,7 +11,7 @@ def search
   else
     param_1 = params[:search][:address]
     param_2 = params[:search][:start_time]
-    param_3 = params[:search][:travel_time].to_i
+    param_3 = 30
     param_4 = params[:search][:level]
     @spots = Spot.near(param_1, param_3).where.not(latitude: nil, longitude: nil)
   end
