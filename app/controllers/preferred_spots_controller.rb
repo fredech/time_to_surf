f372874f3189d750b727333d667ccf1b1a0fd894
Alice Brewer <alicebrewer09@gmail.com>
2019-03-07 10:38:01 +0100

    add cursor to dashboard tab surf sessions

--- app/controllers/preferred_spots_controller.rb
+++ app/controllers/preferred_spots_controller.rb
@@ -11,21 +11,10 @@ class PreferredSpotsController < ApplicationController
     @preferred_spot = PreferredSpot.where(user_id: current_user).find_by_spot_id(params[:id])
     @preferred_spot.destroy
     redirect_back(fallback_location: dashboard_path)
-    # @spot = Spot.find(params[:id])
-    # # @current_user.preferred_spots.pluck(:preferred_spot_id) = @preferred_spot.id
-    # if @preferred_spot.destroy
-    #   respond_to do |format|
-    #     format.html { redirect_to dashboard_path }
-    #     format.js { } # <-- will render `app/views/reviews/create.js.erb`
-    #   end
-    # else
-    #   respond_to do |format|
-    #     format.html { render 'dashboard' }
-    #     format.js  { } # <-- idem
-    #   end
-    # end
-    # redirect_back(fallback_location: dashboard_path)
-    # redirect_to spots_path(start_time: params[:start_time], travel_time: params[:travel_time], address: params[:address])
+    # @session = Session.find params[:session_id]
+    # respond_to do |format|
+    #   format.html { redirect_to dashboard_path }
+    #   format.js
   end
 
   def Preferred_Spot_params
