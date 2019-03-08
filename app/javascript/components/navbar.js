a1d711408ee8702793b0b003db2eba0b1ac1790b
damien arnaud <darnaud@mail.com>
2019-03-08 11:09:08 +0100

    navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,9 +1,9 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar-wagon');
+  const navbar = document.querySelector('.navbar-wagon-right');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight / 30) {
-        navbar.classList.add('navbar-wagon-white');
+        navbar.classList.add('navbar-wagon-right-white');
       } else {
         navbar.classList.remove('navbar-wagon-white');
       }
